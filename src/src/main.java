56681ef21a8de7d1fb1d60f5589a87526cb533f6
flarevar <ishwarchhetri7@outlook.com>
2026-01-18 15:01:26 +0000

    adding a function

--- src/src/main.java
+++ src/src/main.java
@@ -4,6 +4,7 @@ public class main {
     public static void main(String[] args){
         System.out.println("hello yo");
         System.out.println("f");
+        System.out.println("coding is hard right?");
     }
 
 }
